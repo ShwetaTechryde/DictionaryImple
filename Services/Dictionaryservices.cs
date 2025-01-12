fc1051e96b6a2dfb453950ad7b9e7264c5c95bbe
Dell <sweta18singh@gmail.com>
2025-01-12 11:07:51 -0800

    changes

--- Services/Dictionaryservices.cs
+++ Services/Dictionaryservices.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using DictionaryImple.Models;
+using DictionaryImple.Services;
 namespace DictionaryImple.Services
 {
     public class Dictionaryservices
     {
-        
+        Dictionary<int, Emplpyee> empDictionary = new Dictionary<int, Emplpyee>();
+
+        public void AddEmployee(Emplpyee employee)
+        {
+
+            if (empDictionary.ContainsKey(employee.ID))
+            {
+                System.Console.WriteLine($"{employee.ID} Exists"); //interpolation in c#
+            }
+            else
+            {
+                empDictionary.Add(employee.ID, employee);
+                System.Console.WriteLine("Record added in dictionary");
+            }
+        }
+       
     }
 }
--- startup.cs
+++ startup.cs
@@ -2,11 +2,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DictionaryImple.Models;
+using DictionaryImple.Services;
 
 namespace DictionaryImple
 {
     public class Startup
     {
-        
+        public static void Main()
+        {
+            Dictionaryservices Dictionaryservices = new Dictionaryservices();
+
+
+            //Now we can feed the data on that object identifying by the key so that it improves the lookup
+            //clustered index is the best example of Dictionary
+            //Example: Session creates key internally then we can assign the data
+
+            //object initialisaion property
+            try
+            {
+
+
+                Emplpyee employee1 = new Emplpyee
+                {
+                    ID = 103,
+                    Age = int.Parse(Console.ReadLine()),
+                    Name = "Steve"
+                };
+                Emplpyee employee2 = new Emplpyee
+                {
+                    ID = 103,
+                    Age = 30,
+                    Name = "David"
+                };
+
+
+                Dictionaryservices.AddEmployee(employee1);
+                // employeeDictionary.AddEmployee(employee2);
+
+            }
+            catch (FormatException)
+            {
+                System.Console.WriteLine("input is not correct");
+            }
+
+        }
     }
+
 }
