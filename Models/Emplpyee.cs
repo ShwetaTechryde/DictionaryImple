4ff5d1d9145f521b6918ddc82a66c6a5b7b5d96f
Dell <sweta18singh@gmail.com>
2025-01-12 09:47:53 -0800

    Add project files.

---
+++ Models/Emplpyee.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DictionaryImple.Models
+{
+    public class Emplpyee
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
